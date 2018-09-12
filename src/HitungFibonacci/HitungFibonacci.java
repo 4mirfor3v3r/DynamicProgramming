6d84646f4df01760876fa09c7763dbc000857aeb
Ahamad Amirudin 100 <ahmad_amirudin_27rpl@student.smktelkom-mlg.sch.id>
2018-09-12 10:44:37 +0700

    Tambah Input

--- src/HitungFibonacci/HitungFibonacci.java
+++ src/HitungFibonacci/HitungFibonacci.java
@@ -1,5 +1,5 @@
 package HitungFibonacci;
-
+import java.util.*;
 public class HitungFibonacci {
     public static void tampilJudul(String identitas){
         System.out.println("Identitas : "+ identitas);
@@ -10,5 +10,16 @@ public class HitungFibonacci {
     public static void main(String[] args){
         String identitas = "Ahmad Amirudin / X-RPL3 / 02";
         tampilJudul(identitas);
+        
+        int n = tampilInput();
+    }
+    
+    private static int tampilInput(){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Bilangan Ke-: ");
+        int n = input.nextInt();
+        
+        return n;
     }
 }
