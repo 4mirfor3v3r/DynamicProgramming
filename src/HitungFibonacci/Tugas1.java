410769f09405bec7aff85f01effa61aa6aeb0108
Ahamad Amirudin 100 <ahmad_amirudin_27rpl@student.smktelkom-mlg.sch.id>
2018-09-15 11:28:43 +0700

    Deklarasi

---
+++ src/HitungFibonacci/Tugas1.java
@@ -0,0 +1,11 @@
+package HitungFibonacci;
+import java.util.*;
+public class Tugas1 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int[] data ={5000,2000,1000,500,100};
+        int[] hasil = new int[5];
+        int jumlah,x,temp;
+        
+    }
+}
